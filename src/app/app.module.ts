1bab6279665f23f15e3bae84e57287428c703f67
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-10-26 15:14:17 +0100

    feat(translation): implement word by word translation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { NgxChromeStorageModule } from 'ngx-chrome-storage';
-import { RootComponent } from './root/root.component';
-import { QuranComponent } from './quran/quran.component';
-import { GsearchComponent } from './gsearch/gsearch.component';
-import { HijriDateComponent } from './hijri-date/hijri-date.component';
-import { QazaPrayersComponent } from './qaza-prayers/qaza-prayers.component';
-import { CounterInputComponent } from './qaza-prayers/counter-input.component';
-import { PrayerTimingsComponent } from './prayer-timings/prayer-timings.component';
-import { SettingsComponent } from './settings/settings.component';
-import { SettingsConfig } from './settings/settings.class';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
-import { AyatHistoryComponent } from './ayat-history/ayat-history.component';
+import { AppComponent } from "./app.component";
+import { NgxChromeStorageModule } from "ngx-chrome-storage";
+import { RootComponent } from "./root/root.component";
+import { QuranComponent } from "./quran/quran.component";
+import { GsearchComponent } from "./gsearch/gsearch.component";
+import { HijriDateComponent } from "./hijri-date/hijri-date.component";
+import { QazaPrayersComponent } from "./qaza-prayers/qaza-prayers.component";
+import { CounterInputComponent } from "./qaza-prayers/counter-input.component";
+import { PrayerTimingsComponent } from "./prayer-timings/prayer-timings.component";
+import { SettingsComponent } from "./settings/settings.component";
+import { SettingsConfig } from "./settings/settings.class";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+import { HttpClientModule } from "@angular/common/http";
+import { CommonModule } from "@angular/common";
+import { AyatHistoryComponent } from "./ayat-history/ayat-history.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TooltipModule } from "ngx-bootstrap/tooltip";
 
 @NgModule({
   declarations: [
@@ -29,7 +31,7 @@ import { AyatHistoryComponent } from './ayat-history/ayat-history.component';
     CounterInputComponent,
     PrayerTimingsComponent,
     SettingsComponent,
-    AyatHistoryComponent
+    AyatHistoryComponent,
   ],
   imports: [
     CommonModule,
@@ -38,9 +40,11 @@ import { AyatHistoryComponent } from './ayat-history/ayat-history.component';
     ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
-    NgxChromeStorageModule.forRoot(new SettingsConfig())
+    NgxChromeStorageModule.forRoot(new SettingsConfig()),
+    BrowserAnimationsModule,
+    TooltipModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
